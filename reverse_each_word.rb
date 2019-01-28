8f7dc760e2117cd0cdafd47bf6271f38476feb60
Christoph Gruener <christophgruener@me.com>
2019-01-28 21:40:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,5 @@ def reverse_each_word(sentence)
   sentence_array = sentence.split
   reverse_sentence = []
   sentence_array.each {|word| reverse_sentence << word.reverse}
-  reverse_sentence.join
+  reverse_sentence.join(" ")
 end
