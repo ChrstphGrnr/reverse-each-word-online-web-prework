0e5d422555deae37c47526dd4b5f626a2db73c00
Christoph Gruener <christophgruener@me.com>
2019-01-28 21:43:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,6 +5,7 @@ def reverse_each_word(sentence)
   reverse_sentence.join(" ")
 end
 
+
 def reverse_each_word(sentence)
   sentence_array = sentence.split
   (sentence_array.collect {|word|  word.reverse}).join(" ")
